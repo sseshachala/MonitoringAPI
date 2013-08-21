da2cc01f5d15c5f615aabacc335389546e3b1f95
sseshachala <sid4it@gmail.com>
2013-08-21 18:45:14 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,3 +2,8 @@ apt-get update
 apt-get install gdebi
 apt-get install libapache2-mod-python
 apt-get install git
+
+it clone https://github.com/sseshachala/monitoting_api.git
+cd monitoting_api
+
+sudo tar -C /omd/versions/default -xzvf xervrest.tgz
