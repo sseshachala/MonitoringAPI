6f2b743a62c3e6216813a164857435344a84b961
Artemiy Rodionov <artemiy.rodionov@gmail.com>
2013-10-26 23:57:25 +0400

    fix order

--- xervpyapi/app.py
+++ xervpyapi/app.py
@@ -282,7 +282,7 @@ def reload_cmk():
     run("cmk -R")
 
 def inventory_host(host_name):
-    run("cmk -I @%s && cmk -R" % host_name)
+    run("cmk -R && cmk -I @%s" % host_name)
 
 @app.route('/enable_host', methods=["POST"])
 def enable_host():
